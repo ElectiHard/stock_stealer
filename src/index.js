224c72230dd8a9e1e7a9feff05899063f533f170
Aruturuu <47117421+MonarchistycznyArti@users.noreply.github.com>
2021-04-08 09:06:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,23 +7,6 @@ import './styles.css'
 function App() {
   return (
       <Searchbar suggestions={tickers}/>
-    <div>
-      <h1>stock stealer thing</h1>
-      <h2>type to see stocks</h2>
-      <Autocomplete
-        suggestions={[
-          "ALI = F - Aluminium Futures, May-2021",
-          "A - Agilent Technologies, Inc.",
-          "AMCAMC - Entertainment Holdings, Inc",
-          "AAPL - Apple Inc.",
-          "AMZN - Amazon.com, Inc.",
-          "PRIVATE - Ant Group",
-          "AMATApplied Materials, Inc.",
-          "TSLATesla, Inc.",
-          "TSMTaiwan Semiconductor Manufacturing Company Limited"
-        ]}
-      />
-    </div>
   );
 }
 
